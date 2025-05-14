6bf6dd5c89a9e35fa62996f10779d3227b78dba4
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-05-14 02:31:26 -0300

    Authentication class using JWT

---
+++ app/auth.py
@@ -0,0 +1,98 @@
+from datetime import datetime, timedelta
+from typing import Annotated
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from sqlalchemy.testing.pickleable import User
+
+from starlette import status
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from fastapi import Depends, HTTPException, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from db import get_session
+from model.users import User
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+    responses={404: {"description": "Not found"}},
+)
+
+SECRET_KEY = "123"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+db_dependency = Annotated[Session, Depends(get_session)]
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(user_model: CreateUserRequest, db: db_dependency):
+    user_model = User(
+        username=user_model.username,
+        hashed_password=bcrypt_context.hash(user_model.password),
+    )
+    db.add(user_model)
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=30))
+
+    return {"access_token": token, "token_type": "bearer"}
+
+def authenticate_user(db, username: str, password: str) -> User | bool:
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    to_encode = {"sub": username, "id": user_id}
+    expire = datetime.now() + expires_delta
+    to_encode.update({"exp": int(expire.timestamp())})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not authenticated. Could not validate credentials.",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not authenticated. Could not validate credentials."
+        )
+
+user_dependency = Annotated[User, Depends(get_current_user)]
