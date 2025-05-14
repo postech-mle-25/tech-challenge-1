8330482c6087952f87cc6ceee463d3c06fbee86e
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-05-14 02:31:40 -0300

    Implementation of authentication using JWT

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+from fastapi import HTTPException
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
@@ -5,22 +7,25 @@ from sqlmodel import  Session
 from db import get_session, create_db_and_tables
 from routers import processamento
 
+import auth
+from auth import db_dependency, user_dependency
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(_: FastAPI):
     create_db_and_tables()
+    yield
 
+app = FastAPI(lifespan=lifespan)
 
 
 # app.include_router(exportacao.router)
 # app.include_router(importacao.router)
 # app.include_router(geral.router)
 app.include_router(processamento.router)
+app.include_router(auth.router)
 # app.include_router(
 #     admin.router,
 #     prefix="/admin",
@@ -29,7 +34,8 @@ app.include_router(processamento.router)
 #     responses={418: {"description": "I'm a teapot"}},
 # )
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello!"}
+async def root(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    return {"User": user}
