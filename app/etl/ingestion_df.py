2c20f98930f140e0ffb627ad2761e6ffdddd623d
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-05-18 23:24:45 -0300

    Created constants.py and added index_columns list to prevent blank rows on the DB

--- app/etl/ingestion_df.py
+++ app/etl/ingestion_df.py
@@ -2,7 +2,9 @@ from app.etl.cleaning import load_all_data
 from app.db import engine, get_session
 from app.model.tables import Exporta, Importa, Producao, Comercio, Processamento
 from sqlmodel import Session
+import constants
 
+#TODO: remover imports desnecessários
 
 def db_ingestion():
     dfs = load_all_data()
@@ -12,6 +14,12 @@ def db_ingestion():
         for df, Modelo in zip(dfs, modelos):
             # Limpa valores nulos do DataFrame, se necessário
             df = df.where(df.notnull(), None)
+
+            for coluna in constants.DB.INDEX_COLUMNS:
+                if coluna in df.columns:
+                    df = df[df[coluna].notnull()]
+                    df = df[df[coluna].astype(str).str.strip() != '']
+                    df = df[df[coluna].astype(str).str.lower() != 'none']
 
             # Transforma cada linha em um objeto da classe Modelo
             registros = [Modelo(**row) for row in df.to_dict(orient="records")]
