c6ec41b9fd91fc23f263f6a0e4c72aa844ff46a3
Bruno Fernandes <bruno.fernandes.terceiros@claro.com.br>
2025-05-23 14:29:41 -0300

    Tests in development

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 import json
 
 def test_root_unauthenticated():
@@ -6,15 +7,19 @@ def test_root_unauthenticated():
     assert response.status_code == 401
 
 def test_create_new_generic_user():
-    url = "http://127.0.0.1:8000/auth"
-    headers = {"accept": "application/json", "Content-Type": "application/json"}
-    data = {"username": "bruno","password": "test123"}
+    url: str = "http://127.0.0.1:8000/auth"
+    headers: dict = {"accept": "application/json", "Content-Type": "application/json"}
+    data: dict = {"username": "bruno","password": "test123"}
 
-    response = requests.post(url, headers=headers, json=data)
+    response: Response = requests.post(url, headers=headers, json=data)
 
-    response_text_dict = json.loads(response.text)
-    expected_result = ['username', 'id', 'hashed_password']
+    response_json = response.json()
+    expected_result: list[str] = ['username', 'id', 'hashed_password']
     
 
     assert response.status_code == 201
-    assert list(response_text_dict.keys()) == expected_result
+    assert set(response_json.keys()) >= set(expected_result)
+
+    assert isinstance(response_json["username"], str)
+    assert isinstance(response_json["id"], int)
+    assert isinstance(response_json["hashed_password"], str)
