af52923e364441ee4c204b59317eac733fe33a9e
Tamiris <tamirislira@icloud.com>
2025-05-28 23:08:58 -0300

    hotfix

--- app/main.py
+++ app/main.py
@@ -4,27 +4,27 @@ from contextlib import asynccontextmanager
 from sqlmodel import Session
 
 from app.db import get_session, create_db_and_tables
-from app.routers.ingestion import router as ingestion_router
 from app.routers import comercio, exportacao, importacao, producao, processamento
 
 from app import auth
 from app.auth import db_dependency, user_dependency
 
+# from etl.ingestion_df import db_ingestion
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Código executado no startup
     create_db_and_tables()
+    # db_ingestion()
     yield
     # Código executado no shutdown (se necessário)
     print("Application is shutting down...")
 
 app = FastAPI(lifespan=lifespan)
 SessionDep = Annotated[Session, Depends(get_session)]
-app.include_router(ingestion_router, prefix = '/api')
-app.include_router(processamento.router, prefix = '/api')
-app.include_router(auth.router)
 
+app.include_router(auth.router)
+app.include_router(processamento.router, prefix = '/api')
 app.include_router(comercio.router, prefix='/api')
 app.include_router(exportacao.router, prefix='/api')
 app.include_router(importacao.router, prefix='/api')
