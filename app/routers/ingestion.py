0bf07a1585cfe9f565c744dbdc436da51e843596
Tamiris <tamirislira@icloud.com>
2025-05-28 23:08:01 -0300

    changed variables pasta/arquivo to tipo

--- app/routers/ingestion.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.etl.ingestion_df import db_ingestion
-
-router = APIRouter()
-
-@router.post("/ingest")
-def run_ingestion():
-    db_ingestion()
-    return {"message": "Ingestão realizada com sucesso"}
